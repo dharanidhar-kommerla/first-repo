305c3fbdd83a5968e7ade7d6cb268c85a939ac98
dharanidhar-kommerla <dharanidharkommerla2001@gmail.com>
2026-03-02 21:58:16 +0530

    Modified HelloWorld.java. New Functions is added in Calculator class.

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,13 +4,21 @@ class Calculator{
     public static int add(int a, int b){
         return a+b;
     }
+    public static int sub(int a, int b){
+        return a-b;
+    }
 }
 
 public class HelloWorld{
     public static void main(String[] args){
         System.out.println("Hello, World!");
+        
         System.out.println("Adding two numbers");
-        int res = Calculator.add(100, 200);
-        System.out.println("Sum of two numbers = "+res);
+        int res1 = Calculator.add(100, 200);
+        System.out.println("Sum of two numbers = "+res1);
+
+        System.out.println("Subtracting two numbers");
+        int res2 = Calculator.sub(100,500);
+        System.out.println("Subtraction of two numbers = "+res2);
     }
 }
