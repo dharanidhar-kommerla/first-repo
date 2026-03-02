46d28ca6aa3388b5e7b36977fd3b6cc2e1c181ce
dharanidhar-kommerla <dharanidharkommerla2001@gmail.com>
2026-03-02 21:52:40 +0530

    .gitignore file added. Modified HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,16 @@
 import java.util.*;
+
+class Calculator{
+    public static int add(int a, int b){
+        return a+b;
+    }
+}
+
 public class HelloWorld{
     public static void main(String[] args){
         System.out.println("Hello, World!");
+        System.out.println("Adding two numbers");
+        int res = Calculator.add(100, 200);
+        System.out.println("Sum of two numbers = "+res);
     }
 }
